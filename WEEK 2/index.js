9f5598be74f775643c03cd670bc279c8e6948a09
Siddharth230 <siddharthlokhande04@gmail.com>
2024-10-06 18:38:38 +0530

    WEEK 2

---
+++ WEEK 2/index.js
@@ -0,0 +1,11 @@
+const x1 = 0;
+console.log(x1);
+
+const x2 = 202;
+console.log(x2);
+
+const bytes1 = [202, 244, 1, 23];
+console.log(bytes1);
+
+let bytes2 = new Uint8Array([0, 255, 127, 128]);
+console.log(bytes2);
